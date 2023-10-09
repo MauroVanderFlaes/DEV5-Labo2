47e899ab7abb8e9cc4a548488db89985bd310815
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-10-09 15:07:02 +0200

    feat: add islands + move islands

--- Labo2-islands/classes/World.js
+++ Labo2-islands/classes/World.js
@@ -1,4 +1,5 @@
-class World {
+import Island from "./Island.js";
+export default class World {
     constructor() {
       this.islands = []; // a good place to keep track of your islands
       this.hookEvents(); // let's kick things of by hooking up events
@@ -6,6 +7,10 @@ class World {
   
     hookEvents() {
       // hook events like clicking buttons to a specific function
+      //if a element with the id btnAddIsland get clicked, do a function
+        document.getElementById("btnAddIsland").addEventListener("click", () => {
+            this.addIsland();
+        });
     }
   
     save() {
@@ -27,12 +32,28 @@ class World {
       };
     }
   
-    addIsland(island) {
+    addIsland() {
       // add the islands to the DOM
+      let island = new Island();
+      console.log("addIsland");
+      let div = document.createElement("div");
+      div.classList.add("island");
+      div.style.backgroundColor = island.getRandomColor();
+      document.body.appendChild(div);
+      this.moveIsland(div);
     }
   
-    moveIsland(island) {
+    moveIsland(div) {
       // this might be a good point to animate the islands with JS Animations API
+      let coords = this.getCoordinates();
+      div.animate([
+        {transform: `translate(0px, 0px)`},
+        {transform: `translate(${coords.x}px, ${coords.y}px)`}
+
+    ], {
+        duration: 1000,
+        iterations: 1,
+        fill: "forwards"
+    });
     }
   }
-  
